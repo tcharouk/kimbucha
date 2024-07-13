fbac3f857beef8ea81b1c2b70e82af833412bc90
Timothy Charouk <timothycharouk@Timothys-iMac.lan>
2024-07-13 11:02:18 -0700

    changing to react app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber"
+
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
+import Scene from './Scene'
+import Test from './Test'
+
+function App() {
+  return (
+    <Canvas>
+      <ambientLight intensity={4}/>
+      <OrbitControls />
+      <Suspense fallback={null}>
+        <Scene />
+      </Suspense>
+      <Environment preset="sunset" />
+    </Canvas>
+  );
+}
+
+export default App;
